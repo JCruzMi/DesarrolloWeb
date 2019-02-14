04ab51581ae746151847119c25e4ea2fe389663a
jcruzmi08064 <jcruzmi08064@universidadean.edu.co>
2019-02-14 09:26:55 -0500

    implementacion nav v1. errores

---
+++ src/view/Nav.vue
@@ -0,0 +1,19 @@
+<template lang='pug'>
+
+ul.class="topnav"
+  li(a.class="active",href="#") Home
+  li(a.href="#") News
+  li(a.href="#") Contact
+  li(class="right",a.href="#") About
+
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+}
+</script>
+
+<style lang="scss">
+  @import "../scss/nav.scss";
+</style>
